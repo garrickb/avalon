65c7cd7a2e55b23cb08e25a467332497c50499a1
Garrick <garrick.buckley@salesforce.com>
2018-11-03 21:18:29 -0700

    Prod config fixes.

--- server/config/prod.exs
+++ server/config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :avalon, AvalonWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "avalon.garrick.codes", port: 4000],
+  url: [host: "avalon.garrick.codes", port: 4000]
   #cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
