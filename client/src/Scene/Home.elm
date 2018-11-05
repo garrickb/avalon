6316feab3b044f5d1ce2ba8ea87ba45581bb3070
Garrick <garrick.buckley@salesforce.com>
2018-11-04 21:14:58 -0800

    Redesign home page.

--- client/src/Scene/Home.elm
+++ client/src/Scene/Home.elm
@@ -1,12 +1,18 @@
 module Scene.Home exposing (ExternalMsg(..), Model, Msg, init, update, view)
 
 import Bootstrap.Button as Button
+import Bootstrap.Card as Card
+import Bootstrap.Card.Block as Block
 import Bootstrap.Form as Form
 import Bootstrap.Form.Input as Input
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row
 import Data.Session exposing (Session)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Html.Events exposing (keyCode, on, onClick, onInput)
+import Json.Decode as JD
 import Route exposing (Route)
 
 
@@ -28,19 +34,39 @@ init =
 -- VIEW --
 
 
+onKeyDown : (Int -> msg) -> Html.Attribute msg
+onKeyDown tagger =
+    on "keydown" (JD.map tagger keyCode)
+
+
 view : Session -> Model -> Html Msg
 view session model =
-    div []
-        [ h1 [] [ text "Join a Room" ]
-        , Form.group []
-            [ Form.label [] [ text "Username" ]
-            , Input.text [ Input.attrs [ value model.userName, onInput InputUserName, placeholder "Username" ] ]
+    Grid.row
+        [ Row.centerXs, Row.attrs [ style [ ( "height", "100vh" ), ( "overflow", "auto" ) ] ] ]
+        [ Grid.col [ Col.middleXs ]
+            [ h3 [ style [ ( "text-align", "center" ) ] ] [ text "Welcome to Avalon" ]
+            , Card.config []
+                |> Card.block []
+                    [ Block.text []
+                        [ Form.group []
+                            [ Form.label [] [ text "Username" ]
+                            , Input.text [ Input.attrs [ value model.userName, onInput InputUserName, placeholder "Username" ] ]
+                            ]
+                        , Form.group []
+                            [ Form.label [] [ text "Room Name" ]
+                            , Input.text [ Input.attrs [ value model.roomName, onInput InputRoomName, onKeyDown RoomNameKeyDown, placeholder "Room Name" ] ]
+                            ]
+                        , div [ class "text-center" ] [ Button.button [ Button.primary, Button.attrs [ onClick JoinRoom ] ] [ text "Join Room" ] ]
+                        ]
+                    ]
+                |> Card.view
+            , div [ class "text-muted text-center font-weight-light", style [ ( "padding-top", "5%" ) ] ]
+                [ text "Made with "
+                , text "\x1F9D9"
+                , text "by "
+                , a [ href "https://github.com/garrickb" ] [ text "Garrick" ]
+                ]
             ]
-        , Form.group []
-            [ Form.label [] [ text "Room Name" ]
-            , Input.text [ Input.attrs [ value model.roomName, onInput InputRoomName, placeholder "Room Name" ] ]
-            ]
-        , Button.button [ Button.primary, Button.block, Button.attrs [ onClick JoinRoom ] ] [ text "Join Room" ]
         ]
 
 
@@ -51,6 +77,7 @@ view session model =
 type Msg
     = InputRoomName String
     | InputUserName String
+    | RoomNameKeyDown Int
     | JoinRoom
 
 
@@ -67,6 +94,12 @@ update msg model =
 
         InputUserName name ->
             ( ( { model | userName = name }, Cmd.none ), NoOp )
+
+        RoomNameKeyDown key ->
+            if key == 13 then
+                update JoinRoom model
+            else
+                ( ( model, Cmd.none ), NoOp )
 
         JoinRoom ->
             let
